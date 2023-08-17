5d9480ac473310d231d304b4708128a256ac79e6
Vinicius Santos <vfssantos@MacBook-Air-de-Vinicius.local>
2023-08-16 21:20:02 -0300

    fixing api

--- functions/function-exec.ts
+++ functions/function-exec.ts
@@ -22,10 +22,8 @@ async (props: {
   // add cache busting
   url.searchParams.set("v", v[pathname]);
   // add token
-  console.log('GIT TOKEN', GIT_TOKEN)
   if (GIT_TOKEN) url.searchParams.set("token", GIT_TOKEN);
   // import module
-  console.log(`importing ${url.href}`);
   const mod = await importModule(url.href);
   // run module
   const res = await mod?.default(modules)(params);
