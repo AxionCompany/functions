e7e1d2d4442b7ce02c02cd3e361a7f3c44048c01
Vinicius Santos <vfssantos@MacBook-Air-de-Vinicius.local>
2023-09-01 13:03:54 -0300

    adjustments for cloudfare server

--- functions/ports/denoServe.ts
+++ functions/ports/denoServe.ts
@@ -1,6 +1,7 @@
 export default ({ env, handlers, ...adapters }: any) => {
   const handler = handlers[env.HANDLER_TYPE]({ ...adapters, env });
-  return Deno.serve({ port: env.PORT || 8000 }, async (req: Request) => {
+  const server = Deno.serve({ port: env.PORT || 8000 }, async (req: Request) => {
     return (await handler)(req);
   });
+  return server.finished;
 };
