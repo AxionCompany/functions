9aa73793fa8becaa9e29e7c266f20a8b252cdf22
Vinicius Santos <vfssantos@MacBook-Air-de-Vinicius.local>
2023-09-10 16:10:21 -0300

    general fixes

--- functions/connectors/esm-code-bundler.ts
+++ functions/connectors/esm-code-bundler.ts
@@ -3,7 +3,7 @@
 // permitted, such as Deno Deploy, or when running without `--allow-run`.
 import * as esbuild from "https://deno.land/x/esbuild@v0.17.19/wasm.js";
 import { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.8.1/mod.ts";
-import stripShebang from "https://esm.sh/strip-shebang";
+// import stripShebang from "https://esm.sh/strip-shebang";
 import parseCode from "./esm-code-parser.ts";
 
 let esbuildInitialized = false;
@@ -13,106 +13,113 @@ const DENO_USER_AGENT =
 const isDeno = DENO_USER_AGENT.test(navigator.userAgent);
 const isDenoCLI = isDeno && !!Deno?.run;
 
-const DynamicImport = ({type, useWorker}:any) => async function dynamicImport (content: string) {
-  
-  try {
-    
-    return await import(content);
+const DynamicImport = ({ type, useWorker }: any) =>
+  async function dynamicImport(content: string) {
+    try {
+      // if (content.startsWith("node:")) {
+        return await import(content);
+      // } else {
+      //   throw new Error("Not using regular import");
+      // }
+    } catch (err) {
+      console.log(
+        `Error trying to import ${content} with regular import... Using dynamic instead.`,
+      );
 
-    // throw new Error("Not using regular import");
-  } catch (err) {
-
-    console.log(
-      `Error trying to import ${content} with regular import... Using dynamic instead.`,
-    );
-
-    if (!esbuildInitialized) {
-      esbuildInitialized = true;
-      esbuild.initialize({ worker: useWorker || false });
-    }
-
-    const [denoResolver, denoLoader] = denoPlugins({ loader: "portable" });
-
-    const config:any = {
-      plugins: [
-        denoResolver,
-        denoLoader,
-      ],
-      bundle: true,
-      format: "esm",
-      write: false,
-    }
-
-    let filePath;
-
-    if (type === 'file'){
-      const urlObj = new URL(content);
-      filePath = urlObj.href;
-    } else {
-      filePath = `data:text/javascript;base64,${btoa(content)}`;
-    }
-
-    config.entryPoints = [filePath];
-
-    const result = await esbuild.build(config);
-
-    const outputText = result.outputFiles![0].text;
-
-    const parsedCode = parseCode(stripShebang(outputText));
-
-    const _export: any = {};
-    const dependencies: any = {};
-
-    parsedCode.exports.forEach((item: any) => {
-      _export[item.exportedName] = item.localName;
-    });
-
-    for (const item of parsedCode.imports) {
-      try {
-        item.source = `node:${item.source}`; // TODO: this is considering that only node: imports are are left unimported by esbuild
-        const dep:any = await dynamicImport(item.source);
-        item.specifiers?.forEach((i: any) => {
-          dependencies[i.localName] = dep[i.importedName] || dep;
-        });
-      } catch (err) {
-        console.log(
-          `Not possible to load dependency "${item.source}":\n${err.message}`,
-        );
+      if (!esbuildInitialized) {
+        esbuildInitialized = true;
+        esbuild.initialize({ worker: useWorker || false });
       }
+
+      const [denoResolver, denoLoader] = denoPlugins({ loader: "portable" });
+
+      const config: any = {
+        plugins: [
+          denoResolver,
+          denoLoader,
+        ],
+        bundle: true,
+        format: "esm",
+        write: false,
+      };
+
+      let filePath;
+
+      if (type === "file") {
+        const urlObj = new URL(content);
+        filePath = urlObj.href;
+      } else {
+        filePath = `data:text/javascript;base64,${btoa(content)}`;
+      }
+
+      config.entryPoints = [filePath];
+
+      const result = await esbuild.build(config);
+
+      const outputText = result.outputFiles![0].text;
+
+      const parsedCode = parseCode(
+        // stripShebang(
+        outputText,
+        // )
+      );
+
+      const _export: any = {};
+      const dependencies: any = {};
+
+      parsedCode.exports.forEach((item: any) => {
+        _export[item.exportedName] = item.localName;
+      });
+
+      for (const item of parsedCode.imports) {
+        try {
+          item.source = `node:${item.source}`; // TODO: this is considering that only node: imports are are left unimported by esbuild
+          const dep: any = await dynamicImport(item.source);
+          item.specifiers?.forEach((i: any) => {
+            dependencies[i.localName] = dep[i.importedName] || dep;
+          });
+        } catch (err) {
+          console.log(
+            `Not possible to load dependency "${item.source}":\n${err.message}`,
+          );
+        }
+      }
+
+      const AsyncFunction = async function () {}.constructor;
+
+      const mod = await AsyncFunction(
+        ...Object.keys(dependencies),
+        `${parsedCode.code};return ${
+          JSON.stringify(_export)
+            // remove quotes from values
+            .replace(/"([^(")"]+)":/g, "$1:")
+            // remove quotes from keys
+            .replace(/"([^(")"]+)"/g, "$1")
+        }`,
+      )(...Object.values(dependencies));
+
+      const toStringTaggedExports = Object.assign({
+        [Symbol.toStringTag]: "Module",
+      }, mod);
+
+      const sortedExports = Object.fromEntries(
+        Object.keys(toStringTaggedExports)
+          .sort()
+          .map((key) => [key, toStringTaggedExports[key]]),
+      );
+
+      const prototypedExports = Object.assign(
+        Object.create(null),
+        sortedExports,
+      );
+      const sealedExports = Object.seal(prototypedExports);
+
+      isDenoCLI &&
+        esbuild.stop();
+
+      return sealedExports;
     }
-
-    const AsyncFunction = async function () {}.constructor;
-
-    const mod = await AsyncFunction(
-      ...Object.keys(dependencies),
-      `${parsedCode.code};return ${
-        JSON.stringify(_export)
-          // remove quotes from values
-          .replace(/"([^(")"]+)":/g, "$1:")
-          // remove quotes from keys
-          .replace(/"([^(")"]+)"/g, "$1")
-      }`,
-    )(...Object.values(dependencies));
-
-    const toStringTaggedExports = Object.assign({
-      [Symbol.toStringTag]: "Module",
-    }, mod);
-
-    const sortedExports = Object.fromEntries(
-      Object.keys(toStringTaggedExports)
-        .sort()
-        .map((key) => [key, toStringTaggedExports[key]]),
-    );
-
-    const prototypedExports = Object.assign(Object.create(null), sortedExports);
-    const sealedExports = Object.seal(prototypedExports);
-
-    isDenoCLI && 
-    esbuild.stop();
-
-    return sealedExports;
-  }
-};
+  };
 
 // dynamicImport(
 //   `http://TESTE:123456@localhost:8000/features/rates/search?v=${
@@ -123,5 +130,4 @@ const DynamicImport = ({type, useWorker}:any) => async function dynamicImport (c
 //   console.log(result)
 // }).catch(console.log);
 
-
-export default DynamicImport
+export default DynamicImport;
