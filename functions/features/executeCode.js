ae5bf7f2727b7161b5f1c5280d92cbba9d4327aa
Vinicius Santos <vfssantos@outlook.com>
2023-09-10 22:18:44 -0300

    test fix

--- functions/features/executeCode.js
+++ functions/features/executeCode.js
@@ -3,7 +3,8 @@ import JSCodeInterpreter from "../connectors/js-code-interpreter.ts";
 export default (adapters) => {
   const codeInterpreter = JSCodeInterpreter(adapters);
   return async (props) => {
-    const res = await codeInterpreter({ code: props.code });
+    const mod = await codeInterpreter({ code: props.code });
+    const res = await mod.default(props);
     return res;
   };
 };
