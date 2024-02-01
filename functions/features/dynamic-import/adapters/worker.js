35a8a7eb735d0951a9327ce5f08c982547ac80c8
Vinicius Santos <vfssantos@outlook.com>
2024-02-01 17:21:55 -0300

    fixing deno overrides

--- functions/features/dynamic-import/adapters/worker.js
+++ functions/features/dynamic-import/adapters/worker.js
@@ -2,6 +2,9 @@
 import ModuleExecution from "./module-execution.tsx";
 import responseCallback from "../../../utils/responseCallback.ts";
 
+// Flag to check if overrides have been applied
+let overridesApplied = false;
+
 const denoOverrides = {
   "openKv": ({ currentUrl, originalModule }) => (data) => {
     return originalModule(
@@ -16,15 +19,19 @@ self.onmessage = async (e) => {
   const { __requestId__, currentUrl } = e.data;
   const response = responseCallback(__requestId__, postMessage);
   try {
-    Object.keys(self.Deno).forEach((key) => {
-      const originalModule = self.Deno[key];
-      if (denoOverrides[key]) {
-        self.Deno[key] = denoOverrides[key]({
-          currentUrl,
-          originalModule,
-        });
-      }
-    });
+    // Apply overrides only once
+    if (!overridesApplied) {
+      Object.keys(self.Deno).forEach((key) => {
+        const originalModule = self.Deno[key];
+        if (denoOverrides[key]) {
+          self.Deno[key] = denoOverrides[key]({
+            currentUrl,
+            originalModule,
+          });
+        }
+      });
+      overridesApplied = true; // Set the flag to true after applying overrides
+    }
     self.currentUrl = currentUrl;
     const moduleExecutor = ModuleExecution();
     const chunk = await moduleExecutor(e.data, response, self);
