d71c4ecccc3793a70d11c7f3218c38b6e7001163
Vinicius Santos <vfssantos@outlook.com>
2024-02-05 17:49:02 -0300

    improving jsx logic

--- functions/features/dynamic-import/main.ts
+++ functions/features/dynamic-import/main.ts
@@ -5,54 +5,58 @@ import WorkerManager from "./adapters/worker-manager.ts";
 import ModuleExecution from "./adapters/module-execution.tsx";
 
 export default ({ config, ...dependencies }: any) =>
-async ({ url, pathname, data, params, queryParams, __requestId__ }: {
-  url: URL;
-  pathname: string;
-  params: any | null;
-  data: any | null;
-  queryParams: any | null;
-  __requestId__: string;
-}, response: any | null) => {
-  // get module
-  const { loaderUrl, username, password, loader, useWebWorker = true } = config;
+  async ({ url, pathname, data, params, queryParams, __requestId__ }: {
+    url: URL;
+    pathname: string;
+    params: any | null;
+    data: any | null;
+    queryParams: any | null;
+    __requestId__: string;
+  }, response: any | null) => {
+    // get module
+    const { loaderUrl, username, password, loader, useWebWorker = true } = config;
 
-  // cache busting
-  if (v && !v[pathname] || pathname.endsWith("/___cacheBust___")) {
-    pathname = pathname.replace("/___cacheBust___", "");
-    v[pathname] = Date.now();
-  }
+    // cache busting
+    if (v && !v[pathname] || pathname.endsWith("/___cacheBust___")) {
+      pathname = pathname.replace("/___cacheBust___", "");
+      v[pathname] = Date.now();
+    }
 
-  // create urls
-  const importUrl: URL = new URL(pathname, loaderUrl);
-  const currentUrl: URL = url;
+    // create urls
+    const importUrl: URL = new URL(pathname, loaderUrl);
+    const currentUrl: URL = url;
 
-  // add cache busting
-  if (v) url.searchParams.set("v", v[pathname]);
-  // add auth
-  if (username) url.username = username;
-  if (password) url.password = password;
+    // add cache busting
+    if (v) url.searchParams.set("v", v[pathname]);
+    // add auth
+    if (username) url.username = username;
+    if (password) url.password = password;
 
-  let res: any;
-  let pathParams: any;
+    let res: any;
+    let pathParams: any;
 
-  if (useWebWorker) {
-    res = await WorkerManager({ config })({
-      importUrl,
-      currentUrl,
-      pathParams: params,
-      queryParams,
-      data,
-      __requestId__,
-    }, response);
-  } else {
-    res = await ModuleExecution({ loader, dependencies })({
-      importUrl,
-      currentUrl,
-      ...data,
-      ...queryParams,
-      ...pathParams,
-    }, response);
-  }
+    if (useWebWorker) {
+      try {
+        res = await WorkerManager({ config })({
+          importUrl,
+          currentUrl,
+          pathParams: params,
+          queryParams,
+          data,
+          __requestId__,
+        }, response);
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      res = await ModuleExecution({ loader, dependencies })({
+        importUrl,
+        currentUrl,
+        ...data,
+        ...queryParams,
+        ...pathParams,
+      }, response);
+    }
 
-  return res;
-};
+    return res;
+  };
