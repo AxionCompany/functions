7a0957c0f1725490227aea7b9c6d3b5f7c27e6ae
Vinicius Santos <vfssantos@MacBook-Air-de-Vinicius.local>
2024-02-07 14:27:38 -0300

    jsx improvements, auto restart

--- functions/features/dynamic-import/adapters/jsx-worker.js
+++ functions/features/dynamic-import/adapters/jsx-worker.js
@@ -6,6 +6,7 @@ import { bundle } from "https://deno.land/x/emit/mod.ts";
 import React from "npm:react";
 import tailwindcss from "npm:tailwindcss";
 import postcss from "npm:postcss";
+import * as acorn from "npm:acorn";
 
 const getCss = async (tailwindConfig, html, css) => {
   const inputCss = css || `@tailwind base; @tailwind components; @tailwind utilities;`;
@@ -35,6 +36,7 @@ self.onmessage = async (e) => {
         React,
         getCss,
         bundle,
+        findDefaultExportedVariable
       },
     });
     const chunk = await moduleExecutor(e.data, response, self);
@@ -43,3 +45,19 @@ self.onmessage = async (e) => {
     response.error(err);
   }
 };
+
+// Function to find the default exported variable
+const findDefaultExportedVariable = (code) =>{
+  const ast = acorn.parse(code, {ecmaVersion: 2020, sourceType: "module"});
+  for (const node of ast.body) {
+      if (node.type === 'ExportNamedDeclaration') {
+          // Look for the export default declaration
+          for (const specifier of node.specifiers) {
+              if (specifier.exported.name === 'default') {
+                  return specifier.local.name;
+              }
+          }
+      }
+  }
+  return null; // Return null if no default export found
+}
