be9ac83165a44493613256784003e48005c3717c
Vinicius Santos <vfssantos@MacBook-Air-de-Vinicius.local>
2024-02-15 17:03:03 -0300

    loggins shared modules urls

--- test.ts
+++
@@ -1,3 +0,0 @@
-
-const fileloader = new Worker(new URL("./worker-file-loader.ts", import.meta.url).href, { type: "module" });
-const api = new Worker(new URL("./worker-api.ts", import.meta.url).href, { type: "module" });
