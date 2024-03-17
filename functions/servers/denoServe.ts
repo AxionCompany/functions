5cebda2327810ceeb8a28713c18f799e2821c207
Vinicius Santos <vfssantos@outlook.com>
2024-03-17 18:48:41 -0300

    removing old and unnused files

--- functions/servers/denoServe.ts
+++
@@ -1,8 +0,0 @@
-export default ({ port, env, handlers, ...adapters }: any) => {
-  env = { ...env, ...Deno.env.toObject() }
-  const handler = handlers[env.HANDLER_TYPE]({ ...adapters, env });
-  const server = Deno.serve({ port: (port || env.PORT || 8000) }, async (req: Request) => {
-    return (await handler)(req);
-  });
-  return server.finished;
-};
