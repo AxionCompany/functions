16ad423913688ce81ce777c6ab816bdf12c0dd87
Vinicius Santos <vfssantos@outlook.com>
2024-03-19 23:44:13 -0300

    chaning path parameters so it will work on windows

--- functions/examples/main.js
+++ functions/examples/main.js
@@ -1,1 +1,5 @@
-export default () => "Yeeey!"
+export default () => "Yeeey!" // IF a POST, PUT or DELET request is sent, this is the response.
+
+export const GET = () => { return "Hello World!" } // If a GET request is sent, this is the response
+
+// you can also use POST, PUT, and DELETE methods by exporting them as well.
