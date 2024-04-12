72fd810eb03e2fbf278887e3c096412bb525ae2b
Vinicius Santos <vfssantos@outlook.com>
2024-04-12 16:30:26 -0300

    temporarily removing deno overrides

--- functions/features/isolate/adapters/isolate.ts
+++ functions/features/isolate/adapters/isolate.ts
@@ -19,23 +19,23 @@ const checkOrCreateDir = async (path: string) => {
 
 const port: number = parseInt(Deno.args?.[0]) || 3000;
 
-const denoOverrides: any = {
-    "openKv": ({ currentUrl, originalModule, ...rest }: {
-        currentUrl: string;
-        originalModule: any;
-    }) => async (data: any) => {
-        try {
-            console.log('currentUrl', currentUrl)
-            const basePath = new URL(currentUrl).pathname.split("/").filter(Boolean)[0];
-            const kvDir = `data/${basePath}`;
-            await checkOrCreateDir(kvDir);
-            return originalModule(kvDir + '/kv');
-        } catch (err) {
-            console.log('err in open Deno kv ', err)
-            return originalModule(data)
-        }
-    },
-};
+// const denoOverrides: any = {
+//     "openKv": ({ currentUrl, originalModule, ...rest }: {
+//         currentUrl: string;
+//         originalModule: any;
+//     }) => async (data: any) => {
+//         try {
+//             console.log('currentUrl', currentUrl)
+//             const basePath = new URL(currentUrl).pathname.split("/").filter(Boolean)[0];
+//             const kvDir = `data/${basePath}`;
+//             await checkOrCreateDir(kvDir);
+//             return originalModule(kvDir + '/kv');
+//         } catch (err) {
+//             console.log('err in open Deno kv ', err)
+//             return originalModule(data)
+//         }
+//     },
+// };
 
 const _config = {
     middlewares: {},
@@ -48,18 +48,18 @@ const _config = {
 
             try {
                 // Apply overrides only once
-                if (!overridesApplied) {
-                    Object.keys(globalThis.Deno).forEach((key) => {
-                        const originalModule = globalThis.Deno[key];
-                        if (denoOverrides[key]) {
-                            globalThis.Deno[key] = denoOverrides[key]({
-                                currentUrl,
-                                originalModule,
-                            });
-                        }
-                    });
-                    overridesApplied = true; // Set the flag to true after applying overrides
-                }
+                // if (!overridesApplied) {
+                //     Object.keys(globalThis.Deno).forEach((key) => {
+                //         const originalModule = globalThis.Deno[key];
+                //         if (denoOverrides[key]) {
+                //             globalThis.Deno[key] = denoOverrides[key]({
+                //                 currentUrl,
+                //                 originalModule,
+                //             });
+                //         }
+                //     });
+                //     overridesApplied = true; // Set the flag to true after applying overrides
+                // }
 
                 if (!data.importUrl) {
                     return response.send('ok');
