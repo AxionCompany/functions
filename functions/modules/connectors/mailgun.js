e57964aec93aa0908f5ea77bbe856fa6e5d0515f
Giovanni Romano Neto <giovanni.romano@dex.company>
2024-06-26 15:40:39 -0300

    fix: Adjust mailgun connector

--- functions/modules/connectors/mailgun.js
+++ functions/modules/connectors/mailgun.js
@@ -1,6 +1,6 @@
 import Mailgun from "https://deno.land/x/mailgun@v1.1.0/index.ts";
 
-export default async ({ config }) => {
+export default ({ config }) => {
     // Create a instance using your Mailgun API key and domain
     return new Mailgun({
         key: config.key,
