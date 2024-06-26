9b28882cdfb9676dc0c32db7bb8f6212b5a761d6
Giovanni Romano Neto <vfssantos@outlook.com>
2024-06-26 15:33:40 -0300

    fix: Adjust mailgun connector

--- functions/modules/connectors/mailgun.js
+++ functions/modules/connectors/mailgun.js
@@ -1,17 +1,9 @@
 import Mailgun from "https://deno.land/x/mailgun@v1.1.0/index.ts";
-// import "https://deno.land/std@0.208.0/dotenv/load.ts";
 
-// Create a instance using your Mailgun API key and domain
-const mailgun = new Mailgun({
-    key: Deno.env.get("MAILGUN_KEY"),
-    domain: Deno.env.get("MAILGUN_DOMAIN"),
-});
-
-const sendEmail = async (emailData) => {
-    // Send your message off to Mailgun!
-    return await mailgun.send(emailData);
-}
-
-export default {
-    sendEmail,
+export default async ({ config }) => {
+    // Create a instance using your Mailgun API key and domain
+    return new Mailgun({
+        key: config.key,
+        domain: config.domain,
+    });
 }
