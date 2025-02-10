bdb2f688facb9e8eac38c7109e7316e4e90ef48e
Vinicius Santos <vfssantos@outlook.com>
2025-02-10 17:15:13 -0300

    trigger deploy

--- functions/src/utils/withCache.ts
+++ functions/src/utils/withCache.ts
@@ -14,7 +14,6 @@ const createDirIfNotExists = async (path: string) => {
     } catch (e) { console.log('Error on createDirIfNotExists', e) }
 }
 
-
 const Cache = async (projectId: string, prefix = '') => {
 
     let kv;
